48faed4b1c27b7e820fbb62c1daab20207818fa9
qunwang6 <qunwang6@gmail.com>
2023-08-11 17:11:41 +0800

    www

--- src/init.ts
+++ src/init.ts
@@ -5,8 +5,8 @@ import { answers } from './answers/list'
 import { START_DATE } from './logic/constants'
 import { tryFixAnswer } from './logic/answer-fix'
 
-useTitle(computed(() => `${t('name')} - ${t('description')}`))
-
+// useTitle(computed(() => `${t('name')} - ${t('description')}`))
+useTitle(computed(() => `${name} - ${description}`))
 if (!initialized.value)
   showHelp.value = true
 
